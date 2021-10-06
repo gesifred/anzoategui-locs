c80309ced0d2c29347bb9ebfe42c87095b973371
Wilfred V <gesifred@gmail.com>
2021-10-06 02:00:53 -0400

    adding test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+
 import uniqueRandomArray from 'unique-random-array';
 import anzoateguiCities from './anzoategui-cities.json';
 
 export const all = anzoateguiCities;
 export const random = uniqueRandomArray(anzoateguiCities);
+
+
+export function area (r) {
+    return 3.14 * r * r;
+}
+export function circumference (r) {
+    return 3.14 * 3.14 * r;
+}
---
+++ test/test.js
@@ -0,0 +1,51 @@
+import { area } from '../src/index.js';
+import { all, random } from '../src/index.js'
+import { equal } from 'assert';
+import { expect, assert } from 'chai';
+import { describe } from 'mocha';
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
+describe('Area eq zero', function () {
+    describe('Area=0', function () {
+        it('should return 0 when the are is eq zero', function () {
+            equal(area(0), 0);
+        });
+    });
+});
+
+describe('util tests', function () {
+    it('should pass this canary test', function () {
+        expect(true).to.eql(true);
+    });
+});
+
+describe('anzoategui-names', function () {
+    describe('should be array', function(){
+        it('must be an array of strings', function(){
+            expect(all).to.satisfy(isArrayOfStrings);
+        });
+
+        function isArrayOfStrings(arr){
+            return arr.every((el)=>{
+                return typeof el === 'string';
+            });
+        }
+
+        it('must contain "San Mateo"',()=>{
+            expect(all).to.include("San Mateo");
+        });
+    });
+});
+
+describe('Random City', function() {
+    it('Must Return a random city',()=>{
+            let randomCity = random();
+            expect(all).to.include(randomCity);
+        }) 
+});
